5d951ab784a62548a91db4dcd9dcdf469619744c
RE041943 <RE041943@CERN-RE041943.northamerica.cerner.net>
2015-11-11 15:28:50 +0530

    last

--- Gitgmailtest/src/main/java/gmailtest/NewTest.java
+++ Gitgmailtest/src/main/java/gmailtest/NewTest.java
@@ -2,28 +2,43 @@ package gmailtest;
 
 import java.util.concurrent.TimeUnit;
 
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Logger;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class NewTest {
+	
+	Logger log = Logger.getLogger(NewTest.class);
+	
   @Test
   public void gmailtest() {
 	  System.setProperty("webdriver.chrome.driver", 
 	            "C:\\Users\\RE041943\\Desktop\\information\\Training\\selenium\\Selenium Installers\\To_Work_with_chrome_broswer\\chromedriver.exe"); 
-	 
+	  BasicConfigurator.configure();
 	  WebDriver driver; 
 	  driver = new ChromeDriver();
-	  
-	  driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+	  String Exp_Res = "ssss";
+	  	  driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 	  driver.get("https://gmail.com");
 	  driver.findElement(By.id("Email")).sendKeys("ranjithswrk");
 	  driver.findElement(By.id("next")).click();
 	  driver.findElement(By.xpath(".//*[@id='Passwd']")).sendKeys("Ranjith@143");
 	  driver.findElement(By.xpath(".//*[@id='signIn']")).click();
-	  driver.findElement(By.xpath("//*[@id='gb']/div[1]/div[1]/div[2]/div[4]/div[1]/a/span ")).click();
 	  
-	  driver.findElement(By.cssSelector("#gb_71")).click();
+	  String actual = driver.getTitle();
+	  if (actual.equalsIgnoreCase(Exp_Res))
+		{
+			log.info("PASS");
+		}
+		else
+		{
+			log.error("FAIL");
+			
+		}
+	  
   }
 }
